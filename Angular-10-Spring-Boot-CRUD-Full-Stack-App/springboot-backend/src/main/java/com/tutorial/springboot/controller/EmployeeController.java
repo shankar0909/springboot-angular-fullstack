b8be23ff598f8def09d24e1a049a7f17a38e1b1a
shankar0909 <shankarsharma243@gmail.com>
2021-02-04 22:35:25 -0500

    removing pipeline code buildsec file

--- Angular-10-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/com/tutorial/springboot/controller/EmployeeController.java
+++ Angular-10-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/com/tutorial/springboot/controller/EmployeeController.java
@@ -57,7 +57,7 @@ public class  EmployeeController {
 		
 		employee.setFirstName(employeeDetails.getFirstName());
 		employee.setLastName(employeeDetails.getLastName());
-		employee.setEmailId("hardcoded email");
+		employee.setEmailId(employeeDetails.getEmailId());
 		
 		Employee updatedEmployee = employeeRepository.save(employee);
 		return ResponseEntity.ok(updatedEmployee);
