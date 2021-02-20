4ce554c94e35127d95f069d5173f6b2f0dd99755
shankar0909 <shankarsharma243@gmail.com>
2021-02-20 10:09:38 -0500

    adding changes for converting it into web app

--- Angular-10-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/com/tutorial/springboot/SpringbootBackendApplication.java
+++ Angular-10-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/com/tutorial/springboot/SpringbootBackendApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication
+		//extends SpringBootServletInitializer
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
+
+//To convert to web application - remove below
+	/*@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootBackendApplication.class);
+	}*/
 
 }
